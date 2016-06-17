fc7bf967945e5fab42e267b7c656198dc9f71dfb
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2016-06-17 13:31:02 +0900

    c++ source to execute abaqus

---
+++ spring-test/linear1/mkmake.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  vector<string> lst;
+
+  string line;
+  string target;
+
+  while(cin >> line)
+  {
+    lst.push_back(line);
+  }
+
+  if(lst.size() == 0){
+    cerr << "解析対象候補となるinpファイルがみつかりません." << endl;
+    return -1;
+  }
+
+  if(lst.size() > 1){
+    int i = 1;
+    int res = 0;
+    while(true){
+      cerr << "解析対象のInpファイルはどれですか？"<<endl;
+      cerr << "  0以下: キャンセル" << endl;
+      for(vector<string>::iterator it = lst.begin(); it != lst.end(); it++,i++){
+        cerr << "  " << i << ":" << *it << endl;
+      }
+      cin >> res;
+      if(res < 1) return -1;
+      if(res <= lst.size()) break;
+      cerr << "指定が無効です．" << endl;
+      return -1;
+    }
+    target = lst[res - 1];
+  }else{
+    target = lst[0];
+  }
+
+//  cout << "all: " <<target << " wheels.inp Param1.DAT Param2.DAT Param3.DAT ejrs.for ejrs.ctl FIR_LP_0.01_N105.txt" << endl;
+//  cout << "	abaqus user=ejrs interactive job=" << target.substr(0, target.size() - 4) << endl << endl;
+  cout << "all: " <<target << " uel.for" << endl;
+  cout << "	abaqus user=uel job=" << target.substr(0, target.size() - 4) << endl << endl;
+    
+
+  return 0;
+
+}
+
+
+
